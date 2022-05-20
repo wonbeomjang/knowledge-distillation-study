ac83d3f9adc70f2fe45645f3defd5b54b26f66e0
Jang Wonbeom <jtiger958@gmail.com>
2022-05-21 03:31:58 +0900

    make load_model from url

--- model.py
+++ model.py
@@ -2,19 +2,29 @@ import torch
 import torch.nn as nn
 import torchvision
 import torch.nn.functional as F
+from torch.utils.model_zoo import load_url
+
+model_urls = dict(
+    vgg11="https://github.com/wonbeomjang/parameters/releases/download/parameter/vgg11_cifar100_teacher.pth",
+    resnet18="https://github.com/wonbeomjang/parameters/releases/download/parameter/resnet18_cifar100_teacher.pth",
+    resnet50="https://github.com/wonbeomjang/parameters/releases/download/parameter/resnet50_cifar100_teacher.pth",
+)
 
 
 class VGG11(nn.Module):
     output_size = 512 * 7 * 7
 
-    def __init__(self, pretrained=True, num_classes=100, normalize=True):
+    def __init__(self, pretrained=True, num_classes=100, normalize=True, teacher=False):
         super(VGG11, self).__init__()
-        pretrained = torchvision.models.vgg11(pretrained=pretrained)
+        backbone = torchvision.models.vgg11(pretrained=pretrained)
         self.normalize = normalize
 
         for module_name in ['features', 'avgpool']:
-            self.add_module(module_name, getattr(pretrained, module_name))
+            self.add_module(module_name, getattr(backbone, module_name))
         self.linear = nn.Linear(self.output_size, num_classes)
+
+        if pretrained and teacher:
+            self.load_state_dict(load_url(model_urls["vgg11"]))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.features(x)
@@ -27,14 +37,18 @@ class VGG11(nn.Module):
 class ResNet18(nn.Module):
     output_size = 512
 
-    def __init__(self, pretrained=True, num_classes=100, normalize=True):
+    def __init__(self, pretrained=True, num_classes=100, normalize=True, teacher=False):
         super(ResNet18, self).__init__()
-        pretrained = torchvision.models.resnet18(pretrained=pretrained)
+        backbone = torchvision.models.resnet18(pretrained=pretrained)
         self.normalize = normalize
+        self.linear = nn.Linear(self.output_size, num_classes)
 
         for module_name in ['conv1', 'bn1', 'relu', 'maxpool', 'layer1', 'layer2', 'layer3', 'layer4', 'avgpool']:
-            self.add_module(module_name, getattr(pretrained, module_name))
-        self.linear = nn.Linear(self.output_size, num_classes)
+            self.add_module(module_name, getattr(backbone, module_name))
+
+        if pretrained and teacher:
+            self.load_state_dict(load_url(model_urls["resnet18"]))
+
 
     def forward(self, x, get_ha=False):
         x = self.maxpool(self.relu(self.bn1(self.conv1(x))))
@@ -62,14 +76,17 @@ class ResNet18(nn.Module):
 class ResNet50(nn.Module):
     output_size = 2048
 
-    def __init__(self, pretrained=True, num_classes=100, normalize=True):
+    def __init__(self, pretrained=True, num_classes=100, normalize=True, teacher=False):
         super(ResNet50, self).__init__()
-        pretrained = torchvision.models.resnet50(pretrained=pretrained)
+        backbone = torchvision.models.resnet50(pretrained=pretrained)
         self.normalize = normalize
         self.linear = nn.Linear(self.output_size, num_classes)
 
         for module_name in ['conv1', 'bn1', 'relu', 'maxpool', 'layer1', 'layer2', 'layer3', 'layer4', 'avgpool']:
-            self.add_module(module_name, getattr(pretrained, module_name))
+            self.add_module(module_name, getattr(backbone, module_name))
+
+        if pretrained and teacher:
+            self.load_state_dict(load_url(model_urls["resnet50"]))
 
     def forward(self, x, get_ha=False):
         x = self.maxpool(self.relu(self.bn1(self.conv1(x))))
@@ -93,6 +110,3 @@ class ResNet50(nn.Module):
     def __str__(self):
         return "resnet50"
 
-
-if __name__ == "__main__":
-    print(VGG11(False))
